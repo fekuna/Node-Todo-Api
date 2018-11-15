f1e30e35a27bc17ebeb0e44e5c583623185b2437
fekuna <hichirro@gmail.com>
2018-11-15 07:50:51 +0700

    DELETE todo/:id

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.delete('/todos/:id', (req, res) => {
             res.status(404).send();
         }
         
-        res.send(todo)
+        res.send({todo})
     }).catch((err) => {
         res.send(400).send();
     });

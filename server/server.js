4c8ae77cd5c72f87a26fdae963aefdf6070b579a
fekuna <hichirro@gmail.com>
2018-11-15 08:56:29 +0700

    UPDATE todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {user} = require('./models/user');
-var port = process.env.PORT || 3000;
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {user} = require('./models/user');
+const port = process.env.PORT || 3000;
 
 app = express();
 app.use(bodyParser.json())
@@ -63,6 +64,32 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
         res.send(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.compelted) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.compelted = false;
+        body.compeltedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
+    });
+
 });
 
 

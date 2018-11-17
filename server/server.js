41dc90633d69fc8c0eb4204ac492022130ee779e
fekuna <hichirro@gmail.com>
2018-11-17 11:47:25 +0700

    Hash user password before saving

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 const port = process.env.PORT;
 
 app = express();
@@ -108,6 +109,10 @@ app.post('/users', (req, res) => {
         res.status(400).send(err);
     });
 
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 
